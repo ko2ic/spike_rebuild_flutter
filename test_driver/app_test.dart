d15507e8e0654b9f42d71d7bba253c9b12fbee51
ko2ic <ko2ic.dev@gmail.com>
2019-07-03 00:11:03 +0900

    add screenshot

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_driver/flutter_driver.dart';
 import 'package:test/test.dart';
 
@@ -12,6 +14,11 @@ void main() {
 
     setUpAll(() async {
       driver = await FlutterDriver.connect();
+      final health = await driver.checkHealth();
+      if(health.status == HealthStatus.bad){
+        fail("Flutter Driver extension disabled");
+      }
+      Directory('./test_driver/screenshots').create();
     });
 
     tearDownAll(() async {
@@ -21,17 +28,27 @@ void main() {
     });
 
     test('setState画面に遷移する', () async {
+      await _screenshot(driver, "MyHomePage.png");
       await driver.tap(listTitleFinder);
+      await _screenshot(driver, "TopPage0.png");
     });
 
     test('最初のカウントは0', () async {
       expect(await driver.getText(counterTextFinder), "0");
+      await _screenshot(driver, "TopPage0_before_countup.png");
     });
 
     test('ボタンを押下してカウントアップ', () async {
       await driver.tap(buttonFinder);
-
       expect(await driver.getText(counterTextFinder), "1");
+      await _screenshot(driver, "TopPage0_after_countup.png");
     });
   });
+}
+
+Future<void> _screenshot(FlutterDriver driver,String fileName) async {
+  await driver.waitUntilNoTransientCallbacks();
+  final pixels = await driver.screenshot();
+  final file = File("./test_driver/screenshots/$fileName");
+  await file.writeAsBytes(pixels);
 }
